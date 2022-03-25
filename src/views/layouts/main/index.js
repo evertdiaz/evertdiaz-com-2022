5070ad899f1beb33c8d51d13d3da16a7a13a8047
Evert Diaz <evertdiazb@gmail.com>
2022-03-25 03:44:48 -0500

    separated structured cleaned to start

---
+++ src/views/layouts/main/index.js
@@ -0,0 +1,15 @@
+import Footer from "../footer";
+import Header from "../header";
+
+const MainLayout = ({ children }) => {
+
+  return (
+    <div>
+      <Header/>
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
